a0c6fcf3062f93dd55b1b63f5ad2fdd6bd588de1
Asad <amelibaev@ucdavis.edu>
2024-10-26 18:40:49 -0700

    updated

--- Code/XAxisRailControl/XAxisRailControl.ino
+++ Code/XAxisRailControl/XAxisRailControl.ino
@@ -1,11 +1,16 @@
+/*
+Submilimeter X-Axis gantry control code by Asad Melibaev
+*/
+
 #include <AccelStepper.h>
 #include <math.h>
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846 // Simply the value of PI
+#define RC 0.1466275659824047 // Rotary calibration constant calculated by A.M
 
 // Define pins for the stepper motor
 const int stepPin = 13;    // STEP pin connected to pin 13
 const int dirPin = 12;     // DIR pin connected to pin 12
-const int enablePin = 11;   // EN pin connected to pin 9 (optional)
+const int enablePin = 9;   // EN pin connected to pin 9 (optional)
 
 // Define pins for the encoder
 const int encoderPinA = 21; // Encoder Channel A
@@ -17,6 +22,7 @@ AccelStepper stepper(1, stepPin, dirPin);
 // Variables for encoder position
 volatile long encoderPosition = 0; // Current position
 volatile bool lastAState = LOW;     // Last state of encoder pin A
+volatile int offsetFromExpected = 0;
 
 // Interrupt service routine to handle encoder changes
 void updateEncoder() {
@@ -34,18 +40,42 @@ void updateEncoder() {
     lastAState = currentAState; // Update last state
 }
 
+void correctLocation(int offsetFromExpected){
+    stepper.moveTo(offsetFromExpected); // Set target position
+    while (stepper.distanceToGo() != 0) { // Move until the target position is reached and verified by encoder
+        stepper.run(); // Run the stepper motor
+    }
+}
+
+void verifiedAccuracy(int distance, int pulses){
+  double TOLERANCE = 0.1; // in mm
+
+  if (fabs(encoderPosition * RC - distance) > TOLERANCE){
+    offsetFromExpected = pulses*1.33351439541 - encoderPosition;
+  }
+}
+
 void move_rail_x(int distance) {
     int pulses = (distance / (2 * PI * 28)) * 3200; // Calculate pulses for the given distance
     stepper.moveTo(pulses); // Set target position
-    while (stepper.distanceToGo() != 0) { // Move until the target position is reached
+    while (stepper.distanceToGo() != 0) { // Move until the target position is reached and verified by encoder
         stepper.run(); // Run the stepper motor
     }
+
+    /*
+    verifiedAccuracy(distance, pulses);
+
+    if (offsetFromExpected != 0){
+      correctLocation(offsetFromExpected);
+      offsetFromExpected = 0;
+    }
+    */
 }
 
 void setup() {
     Serial.begin(115200);
     pinMode(enablePin, OUTPUT); // Set the enable pin as an output
-    digitalWrite(enablePin, HIGH); // Enable the driver
+    digitalWrite(enablePin, LOW); // Enable the driver
     
     // Set encoder pins as inputs with pull-ups
     pinMode(encoderPinA, INPUT_PULLUP);
@@ -69,7 +99,7 @@ void loop() {
 
     // Optionally, print encoder position for debugging
     Serial.print("Encoder Position: ");
-    Serial.print(encoderPosition*0.1466275659824047);
+    Serial.print(encoderPosition*RC);
     Serial.println(" mm");
     
     delay(1000); // Wait for 1 second before the next move
